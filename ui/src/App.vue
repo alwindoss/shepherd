1342944bdb2d6704d1d65d02f8c2c8ddf1d72e92
Im Kernel <imkernel84@gmail.com>
2020-07-19 11:08:36 +0530

    Configured with router-view
    Signed-off-by: Im Kernel <imkernel84@gmail.com>

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -28,18 +28,7 @@
 
     <v-main>
       <v-container class="fill-height" fluid>
-        <v-row align="center" justify="center">
-          <v-col class="shrink">
-            <v-tooltip right>
-              <template v-slot:activator="{ on }">
-                <v-btn :href="source" icon large target="_blank" v-on="on">
-                  <v-icon large>mdi-code-tags</v-icon>
-                </v-btn>
-              </template>
-              <span>Source</span>
-            </v-tooltip>
-          </v-col>
-        </v-row>
+        <router-view></router-view>
       </v-container>
     </v-main>
 
