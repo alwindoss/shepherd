fd09fc295ae97c888ed0dfce9b961f3e8cdfbdef
Alwin Doss <alwindoss84@gmail.com>
2025-07-05 22:33:25 +0530

    updated the navigation
    Signed-off-by: Alwin Doss <alwindoss84@gmail.com>

--- ui/src/components/NavBar.vue
+++ ui/src/components/NavBar.vue
@@ -1,10 +1,16 @@
 <template>
     <nav>
         <RouterLink to="/">Home</RouterLink>
-        <div>|</div>
+        <span class="separator">|</span>
         <RouterLink to="/about">About</RouterLink>
       </nav>
 </template>
+<style lang="css">
+.separator {
+  margin-left: 5px;
+  margin-right: 5px;
+}
+</style>
 
 <script setup lang="ts">
 </script>
